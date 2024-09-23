1d1f19974e18fe80e66408b5ca5a936ddb05aa78
Dhinesh Kaviraaj <dhineshkaviraaj@gmail.com>
2024-09-23 23:46:38 +0530

    Time: 4 ms (60.12%), Space: 20.2 MB (5.46%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -10,32 +10,16 @@
  * };
  */
 class Solution {
-public: 
-    TreeNode* f(int preStart, int preEnd, int inStart, int inEnd, map<int,int>&mp, 
-                vector<int>& preorder, vector<int>& inorder){
-        
-        if(preStart>preEnd || inStart>inEnd)    return NULL;
-        TreeNode* root=new TreeNode(preorder[preStart]);
-
-        int inRoot=mp[root->val];
-        int numsLeft=inRoot-inStart;
-
-        root->left=f(preStart+1,preStart+numsLeft,inStart,inRoot-1,mp,preorder,inorder);
-        root->right=f(preStart+numsLeft+1,preEnd,inRoot+1,inEnd,mp,preorder,inorder);
-
-        return root;
-    }
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int n=preorder.size();
-        map<int,int>mp;
-        for(int i=0;i<n;i++)
-            mp[inorder[i]]=i;
-        
-        return f(0,n-1,0,n-1,mp,preorder,inorder);
+public:
+    TreeNode* f(vector<int>a, int &ind, int upperBound){
+        if(ind>=a.size() || a[ind]>upperBound)  return NULL;
+        TreeNode* node=new TreeNode(a[ind++]);
+        node->left=f(a,ind,node->val);
+        node->right=f(a,ind,upperBound);
+        return node;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        vector<int> inorder=preorder;
-        sort(inorder.begin(),inorder.end());
-        return buildTree(preorder,inorder);
+        int ind=0;
+        return f(preorder,ind,INT_MAX);
     }
 };
