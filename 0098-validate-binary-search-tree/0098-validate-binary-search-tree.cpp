6168ed4ac57e2d3fd0c63af038c3ecf0084c87a6
Dhinesh Kaviraaj <dhineshkaviraaj@gmail.com>
2024-09-23 22:38:20 +0530

    Time: 10 ms (41.22%), Space: 20.2 MB (95.31%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,20 +11,17 @@
  */
 class Solution {
 public:
-    // Inorder traversal
-    void f(TreeNode* node, vector<int>& ans){
-        if(!node)   return; 
-        f(node->left,ans);
-        ans.push_back(node->val);
-        f(node->right,ans);
+    bool f(TreeNode* root, long long mini, long long maxi){
+        if(!root)   return true;
+        if(root->val<=mini || root->val>=maxi)  return false;
+        if(root->left && root->val<=root->left->val)     return false;
+        if(root->right && root->val>=root->right->val)   return false;
+        mini=min(mini,(long long)root->val);
+        maxi=max(maxi,(long long)root->val);
+        return f(root->left,mini,root->val) && f(root->right,root->val,maxi);
     }
     bool isValidBST(TreeNode* root) {
         if(!root)   return true;
-        vector<int> inorder;
-        f(root,inorder);
-        int n=inorder.size();
-        for(int i=1;i<n;i++)    
-            if(inorder[i-1]>=inorder[i])     return false;
-        return true;
+        return f(root,LLONG_MIN,LLONG_MAX);
     }
 };
