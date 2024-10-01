81e8b647098d6588b5a797901d62de2695f36bf8
Dhinesh Kaviraaj <dhineshkaviraaj@gmail.com>
2024-10-01 23:49:18 +0530

    Time: 0 ms (100%), Space: 13.9 MB (52.14%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -5,14 +5,9 @@ public:
         while(low<=high){
             int mid=(low+high)/2;
             if(nums[mid]==target)   return mid;
+            
             // Sorted section
-            if(nums[low]<=nums[high]){
-                if(nums[mid]>target)
-                    high=mid-1;
-                else
-                    low=mid+1;
-            }
-            else if(nums[low]<=nums[mid]){
+            if(nums[low]<=nums[mid]){
                 if(target>=nums[low] && target<=nums[mid])
                     high=mid-1;
                 else
